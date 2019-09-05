18fff066f8c75051566f482fb45bb5ccfd046295
Apurva <apurkar1@in.ibm.com>
2019-09-05 15:10:15 +0530

    seach and filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './body/search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule, MatOptionModule,MatSelectModule} from '@angular/material';
+import { FilterComponent } from './body/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
